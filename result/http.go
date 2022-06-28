9c2b861ba0af9643ded02473b5abf0dd3f0e5a19
Robert Kisteleki <kistel@kistel.eu>
2022-06-28 23:20:20 +0200

    rethink how http results are represented

--- result/http.go
+++ result/http.go
@@ -14,34 +14,23 @@ import (
 
 type HttpResult struct {
 	BaseResult
-	Uri          string         `json:"uri"`    //
-	RawHttpReply []RawHttpReply `json:"result"` //
-}
+	Uri             string   //
+	HeaderSize      uint     //
+	Headers         []string //
+	BodySize        uint     //
+	Method          string   //
+	Version         string   //
+	ResultCode      uint     //
+	ReplyTime       float64  //
+	TimeToConnect   float64  //
+	TimeToFirstByte float64  //
+	DnsError        string   //
+	Error           string   //
 
-type RawHttpReply struct {
-	AddressFamily   uint            `json:"af"`         //
-	BodySize        uint            `json:"bsize"`      //
-	DnsError        *string         `json:"dnserr"`     //
-	DestinationAddr netip.Addr      `json:"dst_addr"`   //
-	Error           *string         `json:"err"`        //
-	Headers         *[]string       `json:"header"`     //
-	HeaderSize      uint            `json:"hsize"`      //
-	Method          string          `json:"method"`     //
-	ReadTiming      *HttpReadTiming `json:"readtiming"` //
-	ResultCode      uint            `json:"res"`        //
-	ReplyTime       float64         `json:"rt"`         //
-	SubID           *uint           `json:"subid"`      //
-	SubMax          *uint           `json:"submax"`     //
-	Time            *uniTime        `json:"time"`       //
-	TimeToConnect   float64         `json:"ttc"`        //
-	TimeToFirstByte float64         `json:"ttfb"`       //
-	TimeToResolve   *float64        `json:"ttr"`        //
-	Version         string          `json:"ver"`
-}
-
-type HttpReadTiming struct {
-	Offset    uint `json:"o"` //
-	TimeSince uint `json:"t"` //
+	//ReadTiming *HttpReadTiming `json:"readtiming"` //
+	//SubID         *uint               //
+	//SubMax        *uint           //
+	//Time *uniTime `json:"time"` //
 }
 
 func (result *HttpResult) String() string {
@@ -52,21 +41,13 @@ func (result *HttpResult) String() string {
 
 func (result *HttpResult) DetailString() string {
 	res := result.String() +
-		fmt.Sprintf("\t%d", len(result.RawHttpReply))
-	res += "\t["
-	for _, answer := range result.RawHttpReply {
-		if answer.Error != nil {
-			res += *answer.Error
-		} else {
-			res += fmt.Sprintf("%s\t%d\t%d\t%d",
-				answer.Method,
-				answer.ResultCode,
-				answer.HeaderSize,
-				answer.BodySize,
-			)
-		}
-	}
-	res += "]"
+		fmt.Sprintf("\t\"%s\"\t%s\t%d\t%d\t%d",
+			result.Error,
+			result.Method,
+			result.ResultCode,
+			result.HeaderSize,
+			result.BodySize,
+		)
 	return res
 }
 
@@ -75,12 +56,72 @@ func (result *HttpResult) TypeName() string {
 }
 
 func (http *HttpResult) Parse(from string) (err error) {
-	err = json.Unmarshal([]byte(from), &http)
+	var ihttp httpResult
+	err = json.Unmarshal([]byte(from), &ihttp)
 	if err != nil {
 		return err
 	}
-	if http.Type != "http" {
-		return fmt.Errorf("this is not a HTTP result (type=%s)", http.Type)
+	if ihttp.Type != "http" {
+		return fmt.Errorf("this is not a HTTP result (type=%s)", ihttp.Type)
 	}
+
+	http.BaseResult = ihttp.BaseResult
+	http.Uri = ihttp.Uri
+	if len(ihttp.RawHttpReply) >= 1 {
+		// only deal with response 1 for now
+
+		resp := ihttp.RawHttpReply[0]
+		if resp.Headers != nil {
+			http.Headers = *resp.Headers
+		}
+		http.HeaderSize = resp.HeaderSize
+		http.BodySize = resp.BodySize
+		http.Method = resp.Method
+		http.Version = resp.Version
+		http.ResultCode = resp.ResultCode
+		http.ReplyTime = resp.ReplyTime
+		http.TimeToConnect = resp.TimeToConnect
+		http.TimeToFirstByte = resp.TimeToFirstByte
+		if resp.DnsError != nil {
+			http.DnsError = *resp.DnsError
+		}
+		if resp.Error != nil {
+			http.Error = *resp.Error
+		}
+	}
+
 	return nil
+}
+
+//////////////////////////////////////////////////////
+// internal representation of a ping result
+
+type httpResult struct {
+	BaseResult
+	Uri          string         `json:"uri"`    //
+	RawHttpReply []rawHttpReply `json:"result"` //
+}
+
+type rawHttpReply struct {
+	BodySize        uint            `json:"bsize"`      //
+	DnsError        *string         `json:"dnserr"`     //
+	DestinationAddr netip.Addr      `json:"dst_addr"`   //
+	Error           *string         `json:"err"`        //
+	Headers         *[]string       `json:"header"`     //
+	HeaderSize      uint            `json:"hsize"`      //
+	Method          string          `json:"method"`     //
+	ReadTiming      *httpReadTiming `json:"readtiming"` //
+	ResultCode      uint            `json:"res"`        //
+	ReplyTime       float64         `json:"rt"`         //
+	SubID           *uint           `json:"subid"`      //
+	SubMax          *uint           `json:"submax"`     //
+	Time            *uniTime        `json:"time"`       //
+	TimeToConnect   float64         `json:"ttc"`        //
+	TimeToFirstByte float64         `json:"ttfb"`       //
+	Version         string          `json:"ver"`        //
+}
+
+type httpReadTiming struct {
+	Offset    uint `json:"o"` //
+	TimeSince uint `json:"t"` //
 }
