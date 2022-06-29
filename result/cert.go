dcda51bfb4a8419ae4989c8c5cd58a8b73172cbd
Robert Kisteleki <kistel@kistel.eu>
2022-06-29 21:18:32 +0200

    rethink how sslcert results are represented

--- result/cert.go
+++ result/cert.go
@@ -12,9 +12,105 @@ import (
 	"encoding/pem"
 	"fmt"
 	"log"
+	"strings"
 )
 
 type CertResult struct {
+	BaseResult
+	Error           *string            //
+	Alert           *CertAlert         //
+	Method          string             //
+	ConnectTime     float64            //
+	ReplyTime       float64            //
+	ServerCipher    string             //
+	ProtocolVersion string             //
+	Certificates    []x509.Certificate //
+}
+
+// CertAlert is an error that could be sent by the server
+// see RFC 5246 section 7.2
+type CertAlert struct {
+	Level       uint //
+	Description uint //
+}
+
+const (
+	AlertLevelWarning = iota
+	AlertLevelFatal
+)
+
+func (result *CertResult) String() string {
+	ret := result.BaseString()
+	if result.Error != nil {
+		return ret + fmt.Sprintf("\tERROR: %s", *result.Error)
+	}
+	// TODO: test this
+	if result.Alert != nil {
+		ret += fmt.Sprintf("\tALERT: %d %d", result.Alert.Level, result.Alert.Description)
+	}
+	if result.Alert == nil || result.Alert.Level == AlertLevelWarning {
+		ret += fmt.Sprintf("\t%s\t%s\t%f\t%d",
+			result.Method,
+			result.ProtocolVersion,
+			result.ReplyTime,
+			len(result.Certificates),
+		)
+	}
+	return ret
+}
+
+func (result *CertResult) DetailString() string {
+	ret := result.String()
+	if result.Alert == nil && result.Error == nil {
+		ret += fmt.Sprintf("\t%s", result.ServerCipher)
+	}
+	certs := make([]string, 0)
+	for _, cert := range result.Certificates {
+		certs = append(certs, fmt.Sprintf("<%x %s>	",
+			cert.SerialNumber,
+			cert.Subject,
+		))
+	}
+	ret += "\t[" + strings.Join(certs, "\t") + "]"
+	return ret
+}
+
+func (result *CertResult) TypeName() string {
+	return "sslcert"
+}
+
+func (cert *CertResult) Parse(from string) (err error) {
+	var icert certResult
+	err = json.Unmarshal([]byte(from), &icert)
+	if err != nil {
+		return err
+	}
+	if icert.Type != "sslcert" {
+		return fmt.Errorf("this is not a TLS/SSL certificate result (type=%s)", icert.Type)
+	}
+	cert.BaseResult = icert.BaseResult
+	cert.Alert = icert.Alert
+	cert.Error = icert.Error
+	if icert.Error == nil {
+		cert.Method = *icert.Method
+		cert.ReplyTime = *icert.ReplyTime
+		cert.ServerCipher = *icert.ServerCipher
+		cert.ConnectTime = *icert.ConnectTime
+		cert.ReplyTime = *icert.ReplyTime
+		cert.ProtocolVersion = *icert.ProtocolVersion
+		cert.Certificates, err = icert.Certificates()
+		if err != nil {
+			return nil
+		}
+	}
+
+	return nil
+}
+
+//////////////////////////////////////////////////////
+// API version of an sslcert result
+
+type certResult struct {
 	BaseResult
 	Alert           *CertAlert `json:"alert"`         //
 	Method          *string    `json:"method"`        //
@@ -26,54 +122,7 @@ type CertResult struct {
 	RawCertificates *[]string  `json:"cert"`          //
 }
 
-// CertAlert is an error could be sent by the server
-type CertAlert struct {
-	Level       uint //
-	Description uint //
-}
-
-func (result *CertResult) ShortString() string {
-	certs, _ := result.Certificates()
-	ret := result.BaseString() +
-		valueOrNA("", false, result.Error) +
-		valueOrNA("", false, result.Method) +
-		valueOrNA("", false, result.ProtocolVersion) +
-		valueOrNA("", false, result.ReplyTime) +
-		fmt.Sprintf("\t%d", len(certs))
-	return ret
-}
-
-func (result *CertResult) LongString() string {
-	res := result.ShortString() +
-		valueOrNA("", false, result.ServerCipher)
-	certs, err := result.Certificates()
-	if err != nil || len(certs) == 0 {
-		res += "N/A"
-	} else {
-		res += fmt.Sprintf("\t%s\t%s",
-			certs[0].SerialNumber,
-			certs[0].Subject.CommonName,
-		)
-	}
-	return res
-}
-
-func (result *CertResult) TypeName() string {
-	return "sslcert"
-}
-
-func (cert *CertResult) Parse(from string) (err error) {
-	err = json.Unmarshal([]byte(from), &cert)
-	if err != nil {
-		return err
-	}
-	if cert.Type != "sslcert" {
-		return fmt.Errorf("this is not a TLS/SSL certificate result (type=%s)", cert.Type)
-	}
-	return nil
-}
-
-func (result *CertResult) Certificates() (list []x509.Certificate, err error) {
+func (result *certResult) Certificates() (list []x509.Certificate, err error) {
 	list = make([]x509.Certificate, 0)
 	if result.RawCertificates == nil {
 		return
