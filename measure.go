9671a03fe797081defb855a50c248a44e632bb53
Robert Kisteleki <kistel@kistel.eu>
2023-02-27 21:09:00 +0100

    add baseoptions, trace spec & tests

--- measure.go
+++ measure.go
@@ -37,7 +37,8 @@ type measurementTargetBase struct {
 	Target         string    `json:"target"`
 	Type           string    `json:"type"`
 	AddressFamily  uint      `json:"af"`
-	ResolveOnProbe bool      `json:"resolve_on_probe"`
+	Interval       *uint     `json:"interval,omitempty"`
+	ResolveOnProbe *bool     `json:"resolve_on_probe,omitempty"`
 	Tags           *[]string `json:"tags,omitempty"`
 	Spread         *uint     `json:"spread,omitempty"`
 	SkipDNSCheck   *bool     `json:"skip_dns_check,omitempty"`
@@ -45,21 +46,51 @@ type measurementTargetBase struct {
 
 type measurementTargetPing struct {
 	measurementTargetBase
-	Packets        uint  `json:"packets"`
-	PacketSize     uint  `json:"packet_size"`
-	Interval       *uint `json:"interval,omitempty"`
+	Packets        *uint `json:"packets,omitempty"`
+	PacketSize     *uint `json:"packet_size,omitempty"`
+	PacketInterval *uint `json:"packet_interval,omitempty"`
 	IncludeProbeID *bool `json:"include_probe_id,omitempty"`
 }
 
-// various measuerement options
-type PingOptions struct {
+type measurementTargetTrace struct {
+	measurementTargetBase
+	Protocol        string `json:"protocol"`
+	ResponseTimeout *uint  `json:"response_timeout,omitempty"`
+	Packets         *uint  `json:"packets,omitempty"`
+	PacketSize      *uint  `json:"packet_size,omitempty"` // ?
+	ParisId         uint   `json:"paris,omitempty"`
+	FirstHop        *uint  `json:"first_hop,omitempty"`
+	LastHop         *uint  `json:"max_hops,omitempty"`
+	DestinationEH   *uint  `json:"destination_option_size,omitempty"`
+	HopByHopEH      *uint  `json:"hop_by_hop_option_size,omitempty"`
+	DontFragment    *bool  `json:"dont_fragment,omitempty"`
+}
+
+// various measurement options
+type BaseOptions struct {
+	ResolveOnProbe bool
+	Interval       uint
 	Tags           []string
 	Spread         uint
 	SkipDNSCheck   bool
-	Packets        uint // default: 3
-	PacketSize     uint // default: 48 bytes
-	Interval       uint // Time between packets (ms)
+}
+type PingOptions struct {
+	Packets        uint // API default: 3
+	PacketSize     uint // API default: 48 bytes
+	PacketInterval uint // Time between packets (ms)
 	IncludeProbeID bool // Include the probe ID (encoded as ASCII digits) as part of the payload
+}
+type TraceOptions struct {
+	Protocol        string // default: UDP
+	ResponseTimeout uint   // API default: 4000
+	Packets         uint   // API default: 3
+	PacketSize      uint   // API default: 48 bytes
+	ParisId         uint   // API default: 16, default: 0
+	FirstHop        uint   // API default: 1
+	LastHop         uint   // API default: 32
+	DestinationEH   uint   // API default: 0
+	HopByHopEH      uint   // API default: 0
+	DontFragment    bool   // API default: false
 }
 
 type measurementProbeDefinition struct {
@@ -75,6 +106,7 @@ type measurementProbeDefinitionTags struct {
 }
 
 var areas = []string{"WW", "West", "North-Central", "South-Central", "North-East", "South-East"}
+var protocols = []string{"ICMP", "UDP", "TCP"}
 
 func NewMeasurementSpec() (spec *MeasurementSpec) {
 	spec = new(MeasurementSpec)
@@ -138,7 +170,7 @@ func (spec *MeasurementSpec) AddProbesCountry(cc string, n int) error {
 }
 
 func (spec *MeasurementSpec) AddProbesList(list []uint) error {
-	return spec.AddProbesListWitTags(list, nil, nil)
+	return spec.AddProbesListWithTags(list, nil, nil)
 }
 
 func (spec *MeasurementSpec) AddProbesReuse(msm uint, n int) error {
@@ -159,7 +191,7 @@ func (spec *MeasurementSpec) AddProbesCountryWithTags(cc string, n int, tagsincl
 	return spec.addProbeSet("cc", cc, n, tagsincl, tagsexcl)
 }
 
-func (spec *MeasurementSpec) AddProbesListWitTags(list []uint, tagsincl *[]string, tagsexcl *[]string) error {
+func (spec *MeasurementSpec) AddProbesListWithTags(list []uint, tagsincl *[]string, tagsexcl *[]string) error {
 	n := len(list)
 	if n == 0 {
 		return fmt.Errorf("probe list cannot be empty")
@@ -178,26 +210,9 @@ func (def *measurementTargetBase) addCommonFields(
 	typ string,
 	description string,
 	target string,
-	resolveOnProbe bool,
 	af uint,
-) {
-	// common fields
-	def.Type = typ
-	def.Description = description
-	def.Target = target
-	def.ResolveOnProbe = resolveOnProbe
-	def.AddressFamily = af
-}
-
-func (spec *MeasurementSpec) AddPing(
-	description string,
-	target string,
-	resolveOnProbe bool,
-	af uint,
-	options *PingOptions,
+	baseoptions *BaseOptions,
 ) error {
-	var def = new(measurementTargetPing)
-
 	if description == "" {
 		return fmt.Errorf("description cannot be empty")
 	}
@@ -208,36 +223,117 @@ func (spec *MeasurementSpec) AddPing(
 		return fmt.Errorf("address familty must be 4 or 6")
 	}
 
-	def.addCommonFields("ping", description, target, resolveOnProbe, af)
+	// common fields
+	def.Type = typ
+	def.Description = description
+	def.Target = target
+	def.AddressFamily = af
 
-	// defaults
-	def.PacketSize = 48
-	def.Packets = 3
+	if baseoptions != nil {
+		if baseoptions.ResolveOnProbe {
+			def.ResolveOnProbe = &baseoptions.ResolveOnProbe
+		}
+		def.SkipDNSCheck = &baseoptions.SkipDNSCheck
+		if baseoptions.Interval != 0 {
+			def.Interval = &baseoptions.Interval
+		}
+		if baseoptions.Interval != 0 {
+			def.Interval = &baseoptions.Interval
+		}
+		if baseoptions.Spread != 0 {
+			def.Spread = &baseoptions.Spread
+		}
+		if baseoptions.Tags != nil {
+			def.Tags = &baseoptions.Tags
+		}
+	}
 
-	// options & specific fields
-	if options != nil {
-		if options.SkipDNSCheck {
-			def.SkipDNSCheck = &options.SkipDNSCheck
-		}
-		if options.Tags != nil {
-			def.Tags = &options.Tags
-		}
-		if options.Spread != 0 {
-			def.Spread = &options.Spread
-		}
+	return nil
+}
 
-		// ping specific fields
-		if options.Packets != 0 {
-			def.Packets = options.Packets
+func (spec *MeasurementSpec) AddPing(
+	description string,
+	target string,
+	af uint,
+	baseoptions *BaseOptions,
+	pingoptions *PingOptions,
+) error {
+	var def = new(measurementTargetPing)
+
+	if err := def.addCommonFields("ping", description, target, af, baseoptions); err != nil {
+		return err
+	}
+
+	// ping specific fields
+	if pingoptions != nil {
+		if pingoptions.Packets != 0 {
+			def.Packets = &pingoptions.Packets
 		}
-		if options.PacketSize != 0 {
-			def.PacketSize = options.PacketSize
+		if pingoptions.PacketSize != 0 {
+			def.PacketSize = &pingoptions.PacketSize
 		}
-		if options.Interval != 0 {
-			def.Interval = &options.Interval
+		if pingoptions.PacketInterval != 0 {
+			def.PacketInterval = &pingoptions.PacketInterval
 		}
-		if options.IncludeProbeID {
-			def.IncludeProbeID = &options.IncludeProbeID
+		if pingoptions.IncludeProbeID {
+			def.IncludeProbeID = &pingoptions.IncludeProbeID
+		}
+	}
+
+	spec.apiSpec.Definitons = append(spec.apiSpec.Definitons, def)
+
+	return nil
+}
+
+func (spec *MeasurementSpec) AddTrace(
+	description string,
+	target string,
+	af uint,
+	baseoptions *BaseOptions,
+	traceoptions *TraceOptions,
+) error {
+	var def = new(measurementTargetTrace)
+
+	if err := def.addCommonFields("traceroute", description, target, af, baseoptions); err != nil {
+		return err
+	}
+
+	// explicit defaults
+	def.Protocol = "UDP"
+	def.ParisId = 0
+
+	// trace specific fields
+	if traceoptions != nil {
+		if traceoptions.Protocol != "" &&
+			slices.Contains(protocols, traceoptions.Protocol) {
+			def.Protocol = traceoptions.Protocol
+		}
+		if traceoptions.ResponseTimeout != 0 {
+			def.ResponseTimeout = &traceoptions.ResponseTimeout
+		}
+		if traceoptions.Packets != 0 {
+			def.Packets = &traceoptions.Packets
+		}
+		if traceoptions.PacketSize != 0 {
+			def.PacketSize = &traceoptions.PacketSize
+		}
+		if traceoptions.ParisId != 0 {
+			def.ParisId = traceoptions.ParisId
+		}
+		if traceoptions.FirstHop != 0 {
+			def.FirstHop = &traceoptions.FirstHop
+		}
+		if traceoptions.LastHop != 0 {
+			def.LastHop = &traceoptions.LastHop
+		}
+		if traceoptions.DestinationEH != 0 {
+			def.DestinationEH = &traceoptions.DestinationEH
+		}
+		if traceoptions.HopByHopEH != 0 {
+			def.HopByHopEH = &traceoptions.HopByHopEH
+		}
+		if traceoptions.DontFragment {
+			def.DontFragment = &traceoptions.DontFragment
 		}
 	}
 
@@ -247,6 +343,9 @@ func (spec *MeasurementSpec) AddPing(
 }
 
 func (target *measurementTargetPing) MarshalJSON() (b []byte, e error) {
+	return json.Marshal(*target)
+}
+func (target *measurementTargetTrace) MarshalJSON() (b []byte, e error) {
 	return json.Marshal(*target)
 }
 
--- measure_test.go
+++ measure_test.go
@@ -92,58 +92,95 @@ func TestMeasureTargetGeneral(t *testing.T) {
 	var spec MeasurementSpec
 
 	spec = MeasurementSpec{}
-	err = spec.AddPing("description", "www.google.com", true, 3, nil)
+	err = spec.AddPing("description", "www.google.com", 3, nil, nil)
 	if err == nil {
 		t.Errorf("Invalid address family is accepted")
 	}
-	err = spec.AddPing("description", "www.google.com", true, 4, nil)
+	err = spec.AddPing("description", "www.google.com", 4, nil, nil)
 	if err != nil {
 		t.Errorf("Valid address family is not accepted: %v", err)
 	}
-	err = spec.AddPing("description", "www.google.com", true, 6, nil)
+	err = spec.AddPing("description", "www.google.com", 6, nil, nil)
 	if err != nil {
 		t.Errorf("Valid address family is not accepted: %v", err)
 	}
 
 	spec = MeasurementSpec{}
-	err = spec.AddPing("", "", true, 4, nil)
+	err = spec.AddPing("", "", 4, nil, nil)
 	if err == nil {
 		t.Errorf("Empty description or target is accepted")
 	}
+
+	spec = MeasurementSpec{}
+	err = spec.AddPing("description1", "www.google.com", 4, nil, nil)
+	if err != nil {
+		t.Errorf("Valid ping measurement target spec rejected: %v", err)
+	}
+}
+
+func TestMeasureTargetBase(t *testing.T) {
+	var err error
+	var spec MeasurementSpec
+
+	err = spec.AddPing("description1", "www.meta.com", 6, &BaseOptions{
+		ResolveOnProbe: true,
+		Interval:       999,
+		Tags:           []string{"tag1", "tag2"},
+		Spread:         50,
+		SkipDNSCheck:   true,
+	}, nil)
+	b2, err := spec.apiSpec.Definitons[0].MarshalJSON()
+	if err != nil {
+		t.Fatalf("Ping measurement target spec with options failed to marshal to JSON: %v", err)
+	}
+	if string(b2) != `{"description":"description1","target":"www.meta.com","type":"ping","af":6,"interval":999,"resolve_on_probe":true,"tags":["tag1","tag2"],"spread":50,"skip_dns_check":true}` {
+		t.Errorf("Measurement with base options improperly serialized: %s", string(b2))
+	}
+
 }
 
 func TestMeasureTargetPing(t *testing.T) {
 	var err error
 	var spec MeasurementSpec
 
-	spec = MeasurementSpec{}
-	err = spec.AddPing("description1", "www.google.com", true, 4, nil)
-	if err != nil {
-		t.Errorf("Valid ping measurement target spec rejected: %v", err)
-	}
-	b1, err := spec.apiSpec.Definitons[0].MarshalJSON()
-	if err != nil {
-		t.Fatalf("Ping measurement target spec failed to marshal to JSON: %v", err)
-	}
-	if string(b1) != `{"description":"description1","target":"www.google.com","type":"ping","af":4,"resolve_on_probe":true,"packets":3,"packet_size":48}` {
-		t.Errorf("Measurement (ping) improperly serialized: %s", string(b1))
-	}
-
-	err = spec.AddPing("description2", "www.meta.com", false, 6, &PingOptions{
-		Tags:           []string{"tag1", "tag2"},
-		Spread:         50,
-		SkipDNSCheck:   true,
+	err = spec.AddPing("description2", "www.meta.com", 6, nil, &PingOptions{
 		Packets:        5,
-		PacketSize:     99,
-		Interval:       999,
+		PacketSize:     9,
+		PacketInterval: 99,
 		IncludeProbeID: true,
 	})
-	b2, err := spec.apiSpec.Definitons[1].MarshalJSON()
+	b2, err := spec.apiSpec.Definitons[0].MarshalJSON()
 	if err != nil {
 		t.Fatalf("Ping measurement target spec with options failed to marshal to JSON: %v", err)
 	}
-	if string(b2) != `{"description":"description2","target":"www.meta.com","type":"ping","af":6,"resolve_on_probe":false,"tags":["tag1","tag2"],"spread":50,"skip_dns_check":true,"packets":5,"packet_size":99,"interval":999,"include_probe_id":true}` {
-		t.Errorf("Measurement (ping) improperly serialized: %s", string(b2))
+	if string(b2) != `{"description":"description2","target":"www.meta.com","type":"ping","af":6,"packets":5,"packet_size":9,"packet_interval":99,"include_probe_id":true}` {
+		t.Errorf("Measurement (ping) with options improperly serialized: %s", string(b2))
+	}
+
+}
+
+func TestMeasureTargetTrace(t *testing.T) {
+	var err error
+	var spec MeasurementSpec
+
+	err = spec.AddTrace("description2", "www.meta.com", 6, nil, &TraceOptions{
+		Protocol:        "ICMP",
+		ResponseTimeout: 19,
+		Packets:         5,
+		PacketSize:      9,
+		ParisId:         8,
+		FirstHop:        2,
+		LastHop:         3,
+		DestinationEH:   4,
+		HopByHopEH:      7,
+		DontFragment:    true,
+	})
+	b2, err := spec.apiSpec.Definitons[0].MarshalJSON()
+	if err != nil {
+		t.Fatalf("Trace measurement target spec with options failed to marshal to JSON: %v", err)
+	}
+	if string(b2) != `{"description":"description2","target":"www.meta.com","type":"traceroute","af":6,"protocol":"ICMP","response_timeout":19,"packets":5,"packet_size":9,"paris":8,"first_hop":2,"max_hops":3,"destination_option_size":4,"hop_by_hop_option_size":7,"dont_fragment":true}` {
+		t.Errorf("Measurement (trace) with options improperly serialized: %s", string(b2))
 	}
 
 }
@@ -167,7 +204,7 @@ func TestMeasureSpec(t *testing.T) {
 	}
 
 	spec = MeasurementSpec{}
-	spec.AddPing("ping", "google.com", false, 4, nil)
+	spec.AddPing("ping", "google.com", 4, nil, nil)
 	err = spec.Submit(false)
 	if err == nil {
 		t.Errorf("Measurement spec without probes is accepted")
